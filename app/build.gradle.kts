1110d7c685f9e8a31a03c3ff64d1865fda524189
rajaaijay-crypto <rajaaijay@gmail.com>
2026-09-25 23:20:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.edisonsolar.attendance"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 
     compileOptions {
